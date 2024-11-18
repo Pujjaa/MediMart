5c46062264293248f81babc0b9da5c7affa3fb95
Puja <kumaripujacs@gmail.com>
2024-11-19 02:12:57 +0530

    Add middleware

--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,10 @@ use App\Http\Controllers\Admin\OrderController;
 
 
 //without login
-
 Route::get('/home', [HomeController::class, 'homePage']);
 Route::get('/shop', [HomeController::class, 'shopPage']);
 Route::get('/about', [HomeController::class, 'aboutPage']);
 Route::get('/cart', [HomeController::class, 'cartPage']);;
-
 
 
 //login routes
@@ -39,90 +37,109 @@ Route::get('/deleteAccount',[LoginController::class,'deleteAccount']);
 
 
 //register route
-
 Route::get('/registerView', [RegisterController::class,'regisView']);
 Route::post('/register', [RegisterController::class,'register']);
 
 
 // Admin routes 
-Route::get('/adminHome', [DashboardController::class,'adminHome']);
-Route::get('/adminAbout', [DashboardController::class,'adminAbout']);
-Route::get('/adminMsg', [DashboardController::class,'adminMessage']);
-Route::get('/adminAccount', [DashboardController::class,'accountInfo']);
-Route::get('/adminEdit{id}', [DashboardController::class,'adminEditView']);
-Route::post('/adminEditSub', [DashboardController::class,'adminEdit']);
-Route::get('/chnPass',[DashboardController::class,'chnPass']);
-Route::post('/chnPassSub',[DashboardController::class,'chnPassSub']);
+Route::group(['middleware'=>'verifyLogin'], function(){
+    Route::get('/adminHome', [DashboardController::class,'adminHome']);
+    Route::get('/adminAbout', [DashboardController::class,'adminAbout']);
+    Route::get('/adminMsg', [DashboardController::class,'adminMessage']);
+    Route::get('/adminAccount', [DashboardController::class,'accountInfo']);
+    Route::get('/adminEdit{id}', [DashboardController::class,'adminEditView']);
+    Route::post('/adminEditSub', [DashboardController::class,'adminEdit']);
+    Route::get('/chnPass',[DashboardController::class,'chnPass']);
+    Route::post('/chnPassSub',[DashboardController::class,'chnPassSub']);
+});
 
 
 //admin user control
-Route::get('/userInfo', [UserController::class,'userInfo']);
-Route::get('/block{id}',[UserController::class,'block_user']);
-Route::get('/unblock{id}',[UserController::class,'unblock_user']);
-Route::get('/userOrder{id}',[UserController::class,'userOrder']);
+Route::group(['middleware'=>'verifyLogin'], function(){
+    Route::get('/userInfo', [UserController::class,'userInfo']);
+    Route::get('/block{id}',[UserController::class,'block_user']);
+    Route::get('/unblock{id}',[UserController::class,'unblock_user']);
+    Route::get('/userOrder{id}',[UserController::class,'userOrder']);
+});
 
 
 //admin products
-Route::get('/adminSup', [MedicineController::class,'supView']);
-Route::get('/adminSupVit', [MedicineController::class,'vitView']);
-Route::get('/adminSupMin', [MedicineController::class,'minView']);
-Route::get('/adminSupHer', [MedicineController::class,'herView']);
-Route::get('/adminSupPro', [MedicineController::class,'proView']);
-Route::get('/adminSupProbio', [MedicineController::class,'probioView']);
-Route::get('/adminSupImu', [MedicineController::class,'imuView']);
-Route::get('/addProduct', [MedicineController::class,'addProView']);
-Route::post('/addProSub', [MedicineController::class,'addProSub']);
-Route::get('/med_delete{id}{cat}',[MedicineController::class,'medDelete']);
-Route::get('/editProuct{id}{cat}',[MedicineController::class,'editProView']);
-Route::post('/editProuct',[MedicineController::class,'editProSub']);
-
+Route::group(['middleware'=>'verifyLogin'], function(){
+    Route::get('/adminSup', [MedicineController::class,'supView']);
+    Route::get('/adminSupVit', [MedicineController::class,'vitView']);
+    Route::get('/adminSupMin', [MedicineController::class,'minView']);
+    Route::get('/adminSupHer', [MedicineController::class,'herView']);
+    Route::get('/adminSupPro', [MedicineController::class,'proView']);
+    Route::get('/adminSupProbio', [MedicineController::class,'probioView']);
+    Route::get('/adminSupImu', [MedicineController::class,'imuView']);
+    Route::get('/addProduct', [MedicineController::class,'addProView']);
+    Route::post('/addProSub', [MedicineController::class,'addProSub']);
+    Route::get('/med_delete{id}{cat}',[MedicineController::class,'medDelete']);
+    Route::get('/editProuct{id}{cat}',[MedicineController::class,'editProView']);
+    Route::post('/editProuct',[MedicineController::class,'editProSub']);
+});
 
 // admin order
-Route::get('/orderPending',[OrderController::class,'orderPendingView']);
-Route::get('/approve{id}',[OrderController::class,'penToApp']);
-Route::get('/orderApprove',[OrderController::class,'orderApproveView']);
-Route::get('/deliver{id}',[OrderController::class,'appToDel']);
-Route::get('/orderDeliver',[OrderController::class,'orderDeliverView']);
-Route::get('/orderCancel',[OrderController::class,'orderCancelView']);
-
+Route::group(['middleware'=>'verifyLogin'], function(){
+    Route::get('/orderPending',[OrderController::class,'orderPendingView']);
+    Route::get('/approve{id}',[OrderController::class,'penToApp']);
+    Route::get('/orderApprove',[OrderController::class,'orderApproveView']);
+    Route::get('/deliver{id}',[OrderController::class,'appToDel']);
+    Route::get('/orderDeliver',[OrderController::class,'orderDeliverView']);
+    Route::get('/orderCancel',[OrderController::class,'orderCancelView']);
+});
 
 //Patient routes
-Route::get('/patHome', [PatientProfileController::class,'patHomeView']);
-Route::get('/patAbout', [PatientProfileController::class,'patAboutView']);
-Route::get('/contact', [PatientProfileController::class, 'contactPage']);
-Route::post('/contactsub', [PatientProfileController::class, 'contact']);
+Route::group(['middleware'=>'verifyLogin'], function(){
+    Route::get('/patHome', [PatientProfileController::class,'patHomeView']);
+    Route::get('/patAbout', [PatientProfileController::class,'patAboutView']);
+    Route::get('/contact', [PatientProfileController::class, 'contactPage']);
+    Route::post('/contactsub', [PatientProfileController::class, 'contact']);
+});
+
 
 //Patient Account
-Route::get('/patAccount', [PatientAccController::class,'accountInfo']);
-Route::get('/patEdit', [PatientAccController::class,'patEditView']);
-Route::post('/patEditSub', [PatientAccController::class,'patEdit']);
-Route::get('/patChnPass',[PatientAccController::class,'chnPass']);
-Route::post('/patChnPassSub',[PatientAccController::class,'chnPassSub']);
+Route::group(['middleware'=>'verifyLogin'], function(){
+    Route::get('/patAccount', [PatientAccController::class,'accountInfo']);
+    Route::get('/patEdit', [PatientAccController::class,'patEditView']);
+    Route::post('/patEditSub', [PatientAccController::class,'patEdit']);
+    Route::get('/patChnPass',[PatientAccController::class,'chnPass']);
+    Route::post('/patChnPassSub',[PatientAccController::class,'chnPassSub']);
+});
 
 
 //Patient medicine
-Route::get('/patMedView', [PatMedicineController::class,'patStore']);
-Route::get('/patMedVit', [PatMedicineController::class,'patStoreVit']);
-Route::get('/patMedMin', [PatMedicineController::class,'patStoreMin']);
-Route::get('/patMedHer', [PatMedicineController::class,'patStoreHer']);
-Route::get('/patMedPro', [PatMedicineController::class,'patStorePro']);
-Route::get('/patMedProbio', [PatMedicineController::class,'patStoreProbio']);
-Route::get('/patMedImu', [PatMedicineController::class,'patStoreImu']);
+Route::group(['middleware'=>'verifyLogin'], function(){
+    Route::get('/patMedView', [PatMedicineController::class,'patStore']);
+    Route::get('/patMedVit', [PatMedicineController::class,'patStoreVit']);
+    Route::get('/patMedMin', [PatMedicineController::class,'patStoreMin']);
+    Route::get('/patMedHer', [PatMedicineController::class,'patStoreHer']);
+    Route::get('/patMedPro', [PatMedicineController::class,'patStorePro']);
+    Route::get('/patMedProbio', [PatMedicineController::class,'patStoreProbio']);
+    Route::get('/patMedImu', [PatMedicineController::class,'patStoreImu']);
+});
 
 
 //cart
-Route::get('/patMedSingle{id}',[CartController::class,'patStoreSingle']);
-Route::post('/patCartView', [CartController::class,'patCart']);
-Route::get('/cart',[CartController::class,'cartView']);
-Route::get('/removeCart{id}',[CartController::class,'cartRemove']);
+Route::group(['middleware'=>'verifyLogin'], function(){
+    Route::get('/patMedSingle{id}',[CartController::class,'patStoreSingle']);
+    Route::post('/patCartView', [CartController::class,'patCart']);
+    Route::get('/cart',[CartController::class,'cartView']);
+    Route::get('/removeCart{id}',[CartController::class,'cartRemove']);
+});
+
 
 // patient order
-Route::get('/order',[PatOrderController::class,'orderView']);
-Route::get('/cancel{id}',[PatOrderController::class,'calOrder']);
+Route::group(['middleware'=>'verifyLogin'], function(){
+    Route::get('/order',[PatOrderController::class,'orderView']);
+    Route::get('/cancel{id}',[PatOrderController::class,'calOrder']);
+});
+
 
 //checkout
-Route::get('/checkout',[CheckoutController::class,'checkoutView']);
-Route::post('/checkoutDone',[CheckoutController::class,'checkoutSub']);
-Route::get('/thankYou',[CheckoutController::class,'orderPlaced']);
-
+Route::group(['middleware'=>'verifyLogin'], function(){
+    Route::get('/checkout',[CheckoutController::class,'checkoutView']);
+    Route::post('/checkoutDone',[CheckoutController::class,'checkoutSub']);
+    Route::get('/thankYou',[CheckoutController::class,'orderPlaced']);
+});
 
